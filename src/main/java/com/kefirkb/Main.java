83200e11dde857b24cb0d2e0d458a85908236a62
Sergey Nikiforov <snikiforov@devexperts.com>
2017-10-31 18:47:59 +0300

    Code format

--- src/main/java/com/kefirkb/Main.java
+++ src/main/java/com/kefirkb/Main.java
@@ -4,64 +4,54 @@ public class Main {
 
     public static void main(String[] args) {
         int intArray[] = new int[]{5, 90, 35, 45, 150, 3};
-
         System.out.println("Array Before Bubble Sort");
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.print(intArray[i] + " ");
+        for (int el : intArray) {
+            System.out.print(el + " ");
         }
 
         bubbleSort(intArray);
 
         System.out.println("");
-
         System.out.println("Array After Bubble Sort");
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.print(intArray[i] + " ");
+        for (int el : intArray) {
+            System.out.print(el + " ");
         }
     }
 
-
     private static void bubbleSort(int[] intArray) {
-
         int n = intArray.length;
-        int temp = 0;
-
         iterateOuterCycle(0, n, intArray);
     }
 
     private static void iterateOuterCycle(int i, int length, int[] intArray) {
-
         try {
             byte[] condition = new byte[length - i - 1];
             iterateInternalCycle(1, length - i, intArray);
             iterateOuterCycle(++i, length, intArray);
-        } catch (Exception ex) {
-
+        } catch (Exception ignored) {
+            // do nothing
         }
     }
 
     private static void iterateInternalCycle(int j, int length, int[] intArray) {
-
         try {
             byte[] condition = new byte[length - j - 1];
             swap(intArray, j);
             iterateInternalCycle(++j, length, intArray);
-        } catch (Exception ex) {
-
+        } catch (Exception ignored) {
+            // do nothing
         }
     }
 
     private static void swap(int[] intArray, int j) {
         int temp;
-
         try {
             byte[] condition = new byte[(intArray[j - 1] - intArray[j])];
             temp = intArray[j - 1];
             intArray[j - 1] = intArray[j];
             intArray[j] = temp;
-        } catch (Exception ex) {
-
+        } catch (Exception ignored) {
+            // do nothing
         }
-
     }
 }
